fcfa51e63328858e1c98bf370ed7882f4920a815
GorbunovGHub <pavlixrus@mail.ru>
2024-09-22 16:06:36 +0300

    22/09/24-11-32

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from task2.views import func_template, ClassTemplate
+# from task2.views import func_template, ClassTemplate
+from task3.views import home_page, game_page, cart_page
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', func_template),
+#     path('page2/', ClassTemplate.as_view())
+# ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', func_template),
-    path('page2/', ClassTemplate.as_view())
+    path('', home_page),
+    path('page1/', game_page),
+    path('page2/', cart_page),
 ]
