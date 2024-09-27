71f547b5bdc1959308bbbc54dd79b0bb7f5e4f13
GorbunovGHub <pavlixrus@mail.ru>
2024-09-28 02:18:19 +0300

    28/09/24 20:14

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'UrbanDjango',
+    'task2'
+    # 'task5',
 ]
 
 MIDDLEWARE = [
@@ -55,9 +56,8 @@ ROOT_URLCONF = 'UrbanDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [BASE_DIR / 'templates/second_task'],
-        # 'DIRS': [BASE_DIR / 'templates/third_task'],
-        'DIRS': [BASE_DIR / 'templates/fourth_task'],
+        'DIRS': [BASE_DIR / 'templates/second_task'],
+        # 'DIRS': [BASE_DIR / 'templates/fifth_task'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -14,21 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-# from task2.views import func_template, ClassTemplate
-# from task3.views import home_page, game_page, cart_page
-from task4.views import home_page, game_page, cart_page
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', func_template),
-#     path('page2/', ClassTemplate.as_view())
-# ]
+from task2.views import func_template, ClassTemplate
+# from task5.views import sign_up_by_django , sign_up_by_html
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page),
-    path('page1/', game_page),
-    path('page2/', cart_page),
+    path('', func_template),
+    path('page2/', ClassTemplate.as_view())
 ]
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', home_page),
+#     path('page1/', game_page),
+#     path('page2/', cart_page),
+# ]
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path('', sign_up_by_django),
+#     path('/sing/', sign_up_by_html),
+# ]
